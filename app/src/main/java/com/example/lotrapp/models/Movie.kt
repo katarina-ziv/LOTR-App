46f17b67ad5abcc1d7f029952f04eae341e29997
ajak100 <katarinazivkovicdev@gmail.com>
2022-12-27 08:37:41 +0100

    Created layout for Single Movie and added appropriate code in Movie.kt data class

--- app/src/main/java/com/example/lotrapp/models/Movie.kt
+++ app/src/main/java/com/example/lotrapp/models/Movie.kt
@@ -4,5 +4,11 @@ data class Movie(
     var _id: String,
     var name: String,
     var runtimeInMinutes: Int?,
-    var rottenTomatoesScore: Double?
+    var budgetInMillions: Int?,
+    var academyAwardNominations: Int?,
+    var academyAwardWins: Int?,
+    var rottenTomatoesScore: Double?,
+    var image : String?,
+    var backgroundImage: String?,
+    var description : String?
 )
