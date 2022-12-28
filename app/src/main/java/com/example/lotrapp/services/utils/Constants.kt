2878bdc8972adb7138009871421eebf2451e5e8c
ajak100 <katarinazivkovicdev@gmail.com>
2022-12-28 09:32:12 +0100

    Added LIMIT variable to Constants.kt

---
+++ app/src/main/java/com/example/lotrapp/services/utils/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.lotrapp.services.utils
+
+import com.example.lotrapp.BuildConfig
+
+object Constants {
+
+    const val PREF = "myPrefs"
+    const val USERNAME = "USERNAME"
+    const val API_KEY = BuildConfig.API_KEY
+    const val BASE_URL = "https://the-one-api.dev/v2/"
+    const val LIMIT = 100
+
+}
